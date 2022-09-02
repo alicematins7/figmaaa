800354e68e1dcf833fbf94ffbd1af85222f8f58c
Alice Martins <102804150+alicematins7@users.noreply.github.com>
2022-09-02 07:08:17 -0300

    Delete src directory

--- src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import * as S from "./styles";
-import { IButton } from "interfaces/button.interface";
-
-const Button = ({ children }: IButton) => {
-  return <S.Botao>{children}</S.Botao>;
-};
-
-export default Button;
--- src/pages/Layout/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components"
-
-export const Layout = styled.div`
-  main {
-    min-height: 80vh;
-  }
-`
